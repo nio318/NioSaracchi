16e272cedb4671d7ac8fd617f0a50f2c5d1f1146
nio318 <20038344@studenti.uniupo.it>
2022-05-03 09:29:48 +0200

    Query2

--- EserciziCittà/Query2.sql
+++ EserciziCittà/Query2.sql
@@ -1,4 +1,4 @@
-select genitore.nome,figlio.nome
+select genia.*
 from Genia  join persona genitore on genitore.nome=genia.genitore
         join persona figlio  on figlio.nome=genia.figlio
 where genitore.cittànascita=figlio.cittànascita
