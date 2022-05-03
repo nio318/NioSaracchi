9283beb2cca4d498d91586440f5a33bbdd55166e
nio318 <20038344@studenti.uniupo.it>
2022-05-03 10:55:14 +0200

    Esercizio1

---
+++ EserciziProgrammatori/Esercizio1.sql
@@ -0,0 +1,4 @@
+select programmatore.nome,programma.linguaggio,programma.anno
+from autore join programma on autore.id=programma.id
+            join programmatore on programmatore.codice=autore.codice
+where programma.anno>2000  and programma.linguaggio="Java"
