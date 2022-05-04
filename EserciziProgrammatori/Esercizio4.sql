a7ff53fecd983ecb76cc91aed700fbc578f9c754
nio318 <20038344@studenti.uniupo.it>
2022-05-04 09:15:22 +0200

    esercizio4

--- EserciziProgrammatori/Esercizio4.sql
+++ EserciziProgrammatori/Esercizio4.sql
@@ -1,4 +1,5 @@
-select 
-from autore join programma on autore.id=programma.id
-            join programmatore on programmatore.codice=autore.codice 
- 
+select distinct p1.codice, p2.codice
+from autore join programmatore on programmatore.codice=autore.codice,
+    programma join autore p1 on p1.id=programma.id
+              join autore p2 on p2.id=programma.id
+ where p1.id=p2.id and programma.linguaggio="Python"
