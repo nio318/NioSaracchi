6b259e2bc84e1af7e7abc14121949fd2d4633e3c
nio318 <20038344@studenti.uniupo.it>
2022-05-05 15:09:54 +0200

    5

--- EserciziProgrammatori/Esercizio5.sql
+++ EserciziProgrammatori/Esercizio5.sql
@@ -1,4 +1,7 @@
 select programmatore.*
-from autore join programma on autore.id=programma.id
-            join programmatore on programmatore.codice=autore.codice 
-where  programma.linguaggio="Java" 
+from   autore join programmatore on programmatore.codice=autore.codice
+where  programmatore.codice not in ( select  programmatore.codice
+                                     from  autore join programma on autore.id=programma.id
+                                                  join programmatore on programmatore.codice=autore.codice 
+                                     where programma.id!="Java"
+                                   )
