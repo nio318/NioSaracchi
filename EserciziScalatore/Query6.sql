17b452f325850320446612e96593720a27c1f7f9
nio318 <20038344@studenti.uniupo.it>
2022-05-05 09:51:08 +0200

    6

--- EserciziScalatore/Query6.sql
+++ EserciziScalatore/Query6.sql
@@ -1,4 +1,4 @@
 select scalatore.cf,scalatore.nazionenascita,nazione.continente,scalata.nazione as "Scalata in"
 from scalatore join nazione on nazione.nome=scalatore.nazionenascita
-            join scalata on scalatore.cf=scalata.scalatore
+            left join scalata on scalatore.cf=scalata.scalatore
 where nazione.continente<>"America"
